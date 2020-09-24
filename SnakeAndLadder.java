9633265c8cd14bb5ed05d9c9febfa79a8fdb8371
divyacharani <divyacharaninabha@gmail.com>
2020-09-24 21:52:18 +0530

    Added: Welcome message added

---
+++ SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.snakeAndLadder;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		//Variables
+		int position = 0;
+		
+		//Starting Position
+		System.out.println("Starting Position: "+position);
+	}
+	
+}
