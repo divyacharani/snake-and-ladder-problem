9f2dd44b5c0e7edc57ce59a0c363c2af476b4417
divyacharani <divyacharaninabha@gmail.com>
2020-09-24 22:00:28 +0530

    Added: Start position added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,8 +4,16 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		
+		
+		//Variables
+		int position = 0;
+		
 		//Welcome message
 		System.out.println("Welcome to Snake and Ladder!!");
+		
+		//Starting position
+		System.out.println("Starting Position: "+position); 
+	
 	}
 	
 }
