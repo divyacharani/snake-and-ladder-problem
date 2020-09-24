0089db05318e87db3968c968a86df27b86c905f5
divyacharani <divyacharaninabha@gmail.com>
2020-09-24 23:04:52 +0530

    Added: options checking for no play, ladder or snake added

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.snakeAndLadder;
 
 public class SnakeAndLadder {
+	//Constants
+	public static final int NO_PLAY = 0;
+	public static final int LADDER = 1;
+	public static final int SNAKE = 2; 
 
 	public static void main(String[] args) {
 		
@@ -16,6 +20,23 @@ public class SnakeAndLadder {
 		
 		int numberOnDie = (int)(Math.floor(Math.random()*10) % 6 + 1);
 		System.out.println(numberOnDie);
+		
+		int optionCheck = (int)(Math.floor(Math.random()*10) % 3);
+		System.out.println("Option "+optionCheck); 
+		
+		//Checking of options No Play, Ladder or Snake
+		if(optionCheck==LADDER) {
+			position+= numberOnDie;
+			if(position>100)
+				position-= numberOnDie;
+		}
+		else if(optionCheck==SNAKE) {
+			position-= numberOnDie;
+		}
+		else {
+			position = position;
+		}
+		System.out.println(position);
 	
 	}
 	
